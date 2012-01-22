6a42e0f277407406549b168c791af2bac4ed89ff
Donald Stufft <donald.stufft@gmail.com>
2012-01-21 21:22:04 -0500

    add privatebeta to the settings

--- crate_project/settings/production/base.py
+++ crate_project/settings/production/base.py
@@ -5,3 +5,12 @@ SITE_ID = 3
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 DEFAULT_FROM_EMAIL = "support@crate.io"
+
+MIDDLEWARE_CLASSES += ["privatebeta.middleware.PrivateBetaMiddleware"]
+
+INSTALLED_APPS += ["privatebeta"]
+
+PRIVATE_BETA_ALLOWED_URLS = [
+    "/account/login/",
+    "/account/signup/",
+]
