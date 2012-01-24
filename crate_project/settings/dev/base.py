58206f8dffac6704fc1980b5a4f75b5d1214f641
Donald Stufft <donald.stufft@gmail.com>
2012-01-23 23:52:01 -0500

    change the index name to match others

--- crate_project/settings/dev/base.py
+++ crate_project/settings/dev/base.py
@@ -37,7 +37,7 @@ HAYSTACK_CONNECTIONS = {
     "default": {
         "ENGINE": "haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine",
         "URL": "http://127.0.0.1:9200/",
-        "INDEX_NAME": "crate",
+        "INDEX_NAME": "crate-dev",
     },
 }
 
