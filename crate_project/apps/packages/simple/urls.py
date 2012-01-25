232325c8e858788d497682dc77b808694f4b823b
Donald Stufft <donald.stufft@gmail.com>
2012-01-25 05:43:46 -0500

    add a very simple 404 page for the simple site

--- crate_project/apps/packages/simple/urls.py
+++ crate_project/apps/packages/simple/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 from packages.simple.views import PackageIndex, PackageDetail
 
+handler404 = "packages.simple.views.not_found"
+
 urlpatterns = patterns("",
     url(r"^$", PackageIndex.as_view(), name="simple_package_index"),
     url(r"^(?P<slug>.*)/$", PackageDetail.as_view(), name="simple_package_detail"),
