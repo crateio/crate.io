866b4ffcf891e7b51f52e5bc7a89bb2f4feac67d
Donald Stufft <donald.stufft@gmail.com>
2012-02-03 04:40:59 -0500

    add the start of an api

---
+++ crate_project/apps/packages/api.py
@@ -0,0 +1,23 @@
+from tastypie import fields
+from tastypie.resources import ModelResource
+
+from packages.models import Package, Release
+
+
+class PackageResource(ModelResource):
+    class Meta:
+        queryset = Package.objects.all()
+        resource_name = "package"
+
+
+class ReleaseResource(ModelResource):
+    package = fields.ForeignKey(PackageResource, "package")
+
+    class Meta:
+        queryset = Release.objects.all()
+        resource_name = "release"
+        fields = [
+                    "author", "author_email", "created", "description", "download_uri",
+                    "license", "maintainer", "maintainer_email", "package", "platform",
+                    "requires_python", "summary", "version"
+                ]
