a8f3438d7d0bdab68c8fa543759d896b47c913d6
Donald Stufft <donald.stufft@gmail.com>
2012-02-26 05:57:37 -0500

    add pypi to the hosts conf

--- crate_project/hosts.py
+++ crate_project/hosts.py
@@ -5,4 +5,5 @@ from django_hosts import patterns, host
 host_patterns = patterns("",
     host(r"www", settings.ROOT_URLCONF, name="default"),
     host(r"simple", "packages.simple.urls", name="simple"),
+    host(r"pypi", "pypi.simple.urls", name="pypi"),
 )
