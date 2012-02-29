63aa9be4966a921c43c39052d1da15e0ceb3f34a
Donald Stufft <donald.stufft@gmail.com>
2012-02-28 21:57:16 -0500

    add in the restricted hosts conf

--- crate_project/hosts.py
+++ crate_project/hosts.py
@@ -6,4 +6,5 @@ host_patterns = patterns("",
     host(r"www", settings.ROOT_URLCONF, name="default"),
     host(r"simple", "packages.simple.urls", name="simple"),
     host(r"pypi", "pypi.simple.urls", name="pypi"),
+    host(r"restricted", "packages.simple.restricted_urls", name="restricted"),
 )
