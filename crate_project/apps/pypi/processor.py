6aebffd71a7907b9c4c75f13b8671fa8d4c6b5c2
Donald Stufft <donald.stufft@gmail.com>
2012-03-15 19:10:08 -0400

    do a full clean on a release_file in the download step

--- crate_project/apps/pypi/processor.py
+++ crate_project/apps/pypi/processor.py
@@ -408,6 +408,7 @@ class PyPIPackage(object):
 
                         release_file.digest = "$".join(["sha256", hashlib.sha256(resp.content).hexdigest().lower()])
 
+                        release_file.full_clean()
                         release_file.file.save(file_data["filename"], ContentFile(resp.content), save=True)
 
                         # Store data relating to this file (if modified etc)
