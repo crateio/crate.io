7c6fed4dc08529a281d1694924797e8e4021d73f
Donald Stufft <donald.stufft@gmail.com>
2012-04-03 04:23:34 -0400

    fix and flesh out the admin a bit more

--- crate_project/apps/history/admin.py
+++ crate_project/apps/history/admin.py
@@ -4,9 +4,9 @@ from history.models import Event
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ["package", "version", "data", "created"]
-    list_filter = ["created"]
+    list_display = ["package", "version", "action", "data", "created"]
+    list_filter = ["action", "created"]
     search_fields = ["package", "version"]
 
 
-admin.register(Event, EventAdmin)
+admin.site.register(Event, EventAdmin)
