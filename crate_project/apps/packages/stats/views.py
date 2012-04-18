20cf001dcfab04b04a13064ce134de19623eaf6e
Donald Stufft <donald.stufft@gmail.com>
2012-04-18 05:55:35 -0400

    cache and betterize the stats_delta view

--- crate_project/apps/packages/stats/views.py
+++ crate_project/apps/packages/stats/views.py
@@ -1,4 +1,5 @@
 import collections
+import datetime
 import json
 import time
 
@@ -7,16 +8,28 @@ import isoweek
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
-from packages.models import Package, Release, DownloadDelta
+from packages.models import Package, Release, DownloadDelta, DownloadStatsCache
 
 def stats_delta(request, slug):
     package = get_object_or_404(Package, name=slug)
-    releases = list(Release.objects.filter(package=package).order_by("order"))
+
+    try:
+        cached = DownloadStatsCache.objects.get(package=package)
+    except DownloadStatsCache.DoesNotExist:
+        pass
+    else:
+        return HttpResponse(json.dumps(cached.data), mimetype="application/json")
+
+    releases = Release.objects.filter(package=package)
+    releases = sorted(releases, key=lambda x: x.downloads)
+
+    specific_releases = set([x.version for x in releases[-8:]])
+
     deltas = list(DownloadDelta.objects.filter(file__release__in=releases).order_by("date"))
 
     # @@@ Sanity Checks
 
-    data = [{"name": release.version, "data": []} for release in releases]
+    data = [{"name": "Other", "data": []}] + [{"name": release.version, "data": []} for release in releases if release.version in specific_releases]
 
     # Get First Week
     start_week = isoweek.Week.withdate(deltas[0].date)
@@ -24,7 +37,7 @@ def stats_delta(request, slug):
 
     current = isoweek.Week(start_week.year, start_week.week)
 
-    while current.year <= end_week.year and current.week <= end_week.week:
+    while current.year <= end_week.year and current.week < end_week.week:
         for x in data:
             x["data"].append({"x": int(time.mktime(current.day(0).timetuple()))})
         current = isoweek.Week(current.year, current.week + 1)
@@ -33,11 +46,12 @@ def stats_delta(request, slug):
 
     for d in deltas:
         target = int(time.mktime(isoweek.Week.withdate(d.date).day(0).timetuple()))
-        _data[d.file.release.version][target] = d.delta
+        _data[d.file.release.version if d.file.release.version in specific_releases else "Other"][target] = d.delta
 
     for i in xrange(0, len(data)):
         for j in xrange(0, len(data[i]["data"])):
-            data[i]["data"][j]["y"] = _data[data[i]["name"]].get(data[i]["data"][j]["x"], 0)
+            data[i]["data"][j]["y"] = _data[data[i]["name"] if data[i]["name"] in specific_releases else "Other"].get(data[i]["data"][j]["x"], 0)
 
+    DownloadStatsCache.objects.create(package=package, data=data)
 
     return HttpResponse(json.dumps(data), mimetype="application/json")
