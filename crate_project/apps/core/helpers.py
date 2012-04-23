858ee0f1e6c41812b73edae2f9a4f1fdac0c204f
Donald Stufft <donald.stufft@gmail.com>
2012-04-22 21:47:58 -0400

    add a bootstrap macro that renders a form for bootstrap

--- crate_project/apps/core/helpers.py
+++ crate_project/apps/core/helpers.py
@@ -88,3 +88,13 @@ def static(path):
     using staticfiles' storage backend
     """
     return staticfiles_storage.url(path)
+
+
+@register.filter
+def is_checkbox(field):
+    return field.field.widget.__class__.__name__.lower() == "checkboxinput"
+
+
+@register.filter
+def css_class(field):
+    return field.field.widget.__class__.__name__.lower()
