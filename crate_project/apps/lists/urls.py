4b396b31c62a816cfc114c8dbac727776d793ff2
Donald Stufft <donald.stufft@gmail.com>
2012-05-01 15:45:26 -0400

    add a view to add a package to an existing list

--- crate_project/apps/lists/urls.py
+++ crate_project/apps/lists/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
-from lists.views import RemoveFromList
+from lists.views import AddToList, RemoveFromList
 
 urlpatterns = patterns("",
-    url(r"^(?P<list>[^/]+)/(?P<package>[^/]+)/remove/$", RemoveFromList.as_view(), name="remove_package_from_list")
+    url(r"^(?P<list>[^/]+)/(?P<package>[^/]+)/add/$", AddToList.as_view(), name="add_package_to_list"),
+    url(r"^(?P<list>[^/]+)/(?P<package>[^/]+)/remove/$", RemoveFromList.as_view(), name="remove_package_from_list"),
 )
--- crate_project/apps/lists/views.py
+++ crate_project/apps/lists/views.py
@@ -1,9 +1,7 @@
 import json
 
-from django.core.cache import cache
 from django.http import HttpResponse
 from django.views.generic.base import View
-from django.views.generic.list import ListView
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext as _
 
@@ -11,6 +9,55 @@ from django.contrib.auth.decorators import login_required
 
 from lists.models import List
 from packages.models import Package
+
+
+class AddToList(View):
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(AddToList, self).dispatch(*args, **kwargs)
+
+    def render_json(self, **data):
+        return HttpResponse(json.dumps(data), mimetype="application/json")
+
+    def get_package(self, package):
+        return next(iter(Package.objects.filter(name=package)[:1]), None)
+
+    def get_list(self, list, user):
+        return next(iter(List.objects.filter(name=list, user=user)[:1]), None)
+
+    def post(self, request, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+        package = self.get_package(self.kwargs.get("package"))
+
+        if package is None:
+            return self.render_json(
+                        package=self.kwargs.get("package"),
+                        list=self.kwargs.get("list"),
+                        success=False,
+                        message=_("Package does not exist")
+                    )
+
+        user_list = self.get_list(self.kwargs.get("list"), user=request.user)
+
+        if user_list is None:
+            return self.render_json(
+                        package=self.kwargs.get("package"),
+                        list=self.kwargs.get("list"),
+                        success=False,
+                        message=_("List does not exist")
+                    )
+
+        user_list.packages.add(package)
+
+        return self.render_json(
+                    package=self.kwargs.get("package"),
+                    list=self.kwargs.get("list"),
+                    success=True,
+                    message=_("Successfully removed %(package)s from %(list)s.") % self.kwargs
+                )
 
 
 class RemoveFromList(View):
