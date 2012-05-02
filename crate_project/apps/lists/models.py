fe56212decd8cb18eff14c9371e454171586af95
Donald Stufft <donald.stufft@gmail.com>
2012-05-02 14:18:23 -0400

    add slug to Lists and generate ones for existing lists

--- crate_project/apps/lists/models.py
+++ crate_project/apps/lists/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
 
 from model_utils.models import TimeStampedModel
@@ -8,12 +9,29 @@ class List(TimeStampedModel):
     user = models.ForeignKey("auth.User")
     # Translators: This is used to allow naming a specific list of packages.
     name = models.CharField(_("Name"), max_length=50, db_index=True)
+    slug = models.SlugField(max_length=50)
     private = models.BooleanField(_("Private List"), default=False, help_text=_("Private lists are visible only to you."))
 
     packages = models.ManyToManyField("packages.Package", verbose_name=_("Packages"))
 
     class Meta:
-        unique_together = ("user", "name")
+        unique_together = [
+            ("user", "name"),
+            ("user", "slug"),
+        ]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = slugify(self.name)
+            i = 1
+
+            while List.objects.filter(user=self.user, slug=slug).exists():
+                slug = slugify(u"%s %s" % (self.name, i))
+                i += 1
+
+            self.slug = slug
+
+        return super(List, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return u"%(username)s / %(listname)s" % {"username": self.user.username, "listname": self.name}
