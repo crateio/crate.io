237ec963f7234832d8153e5a0d0b853a049e1ad7
Donald Stufft <donald.stufft@gmail.com>
2012-05-02 14:37:59 -0400

    added a view and template for viewing the details of a list

--- crate_project/apps/lists/views.py
+++ crate_project/apps/lists/views.py
@@ -1,7 +1,9 @@
 import json
 
+from django.db.models import Q
 from django.http import HttpResponse
 from django.views.generic.base import View
+from django.views.generic.detail import DetailView
 from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext as _
 
@@ -119,3 +121,28 @@ class RemoveFromList(View):
         messages.success(request, self.get_message())
 
         return self.render_json(package=kwargs.get("package"), list=kwargs.get("list"), success=True, message=self.get_message())
+
+
+class ListDetail(DetailView):
+
+    queryset = List.objects.all().select_related("packages")
+
+    def get_queryset(self):
+        qs = super(ListDetail, self).get_queryset()
+        qs = qs.filter(user__username=self.kwargs.get("username"))
+
+        if self.request.user.is_authenticated():
+            qs = qs.filter(Q(private=False) | Q(private=True, user=self.request.user))
+        else:
+            qs = qs.filter(private=False)
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super(ListDetail, self).get_context_data(**kwargs)
+
+        ctx.update({
+            "packages": sorted(self.object.packages.all(), key=lambda x: x.latest.created, reverse=True)
+        })
+
+        return ctx
