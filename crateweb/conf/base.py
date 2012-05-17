31d5ee97052e546d7d2b763ef698a789705401ff
Donald Stufft <donald.stufft@gmail.com>
2012-05-17 00:48:39 -0400

    Added crate.pypi to ther equirements

--- crateweb/conf/base.py
+++ crateweb/conf/base.py
@@ -178,6 +178,7 @@ INSTALLED_APPS = [
     "crate.web.history",
     "crate.web.lists",
     "crate.web.utils",
+    "crate.pypi",
 ]
 
 FIXTURE_DIRS = [
--- crateweb/hosts.py
+++ crateweb/hosts.py
@@ -5,6 +5,6 @@ from django_hosts import patterns, host
 host_patterns = patterns("",
     host(r"www", settings.ROOT_URLCONF, name="default"),
     host(r"simple", "crate.web.packages.simple.urls", name="simple"),
-    #host(r"pypi", "pypi.simple.urls", name="pypi"),
+    host(r"pypi", "crate.pypi.simple.urls", name="pypi"),
     host(r"restricted", "crate.web.packages.simple.restricted_urls", name="restricted"),
 )
