75f05f2270bf74949522770a040674f636a40fc2
Donald Stufft <donald@stufft.io>
2013-02-28 07:29:41 -0500

    i

--- crateweb/urls.py
+++ crateweb/urls.py
@@ -37,7 +37,7 @@ urlpatterns = patterns("",
     #url(r"^help/", include("helpdocs.urls")),
     #url(r"^api/", include("crateweb.api_urls")),
 
-    url(r"^status-quo/$", "crate.web.packages.views.fuck_the_status_quo"),
+    url(r"^externally-hosted/$", "crate.web.packages.views.fuck_the_status_quo"),
 
     url(r"^", include("crate.web.search.urls")),
 )
